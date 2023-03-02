9c2ddcd51c430c388ed8f0409d5cb42232f62d6b
Ronit khajuria <94978319+ronitsharma03@users.noreply.github.com>
2023-03-02 22:26:11 +0530

    Update API
    Updated the API that provide weather images and description for the current weather

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'de0142362amsh2d71fa8909a5f88p18b6b4jsnda9c751a81a9',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-};
-
 let cityName = document.getElementById("cityInput");
 let citySearched = document.getElementById("search");
 let submitBtn = document.getElementById("submit");
@@ -15,21 +7,37 @@ submitBtn.addEventListener('click', (e)=>{
 })
 
 
-const getWeather = ((city) => {
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+const apiKey = "94c3a56393535f680509006b9b1b443a"; 
+
+
+
+const getWeather = ((cityName)=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
     .then(response => response.json())
-    .then((response) =>{
-        // console.log(response);
-        (cityName).innerHTML = city;
-        max_temp.innerHTML = response.max_temp + "&#176 C"
-        min_temp.innerHTML = response.min_temp + "&#176 C"
-        temp.innerHTML = response.temp + "&#176 C"
-        wind_speed.innerHTML = response.wind_speed + " Km/h"
+    .then((response) => {
+    //   console.log(response); 
+        cityInput.innerHTML = cityName;
+        temp.innerHTML = response.main.temp + "&#176 C";
+        max_temp.innerHTML = response.main.temp_max + "&#176 C";
+        min_temp.innerHTML = response.main.temp_min + "&#176 C";
+        wind_speed.innerHTML = response.wind.speed + " Km/h";
+        description.innerHTML = response.weather[0].description;
+        const iconCode = response.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+        let imageUrl = document.getElementById("weather-icon-type").src = iconUrl;
+        document.getElementById("weather-icon-type").setAttribute("src", ""+iconUrl);
+
+       
+
+
     })
-	.catch (err => console.error(err));
+    .catch(error => {
+      console.error(error);
+    });
 });
 
 
+// For time of current location
 function startTime() {
     let today = new Date();
     let h = today.getHours();
